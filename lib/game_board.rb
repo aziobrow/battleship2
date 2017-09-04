d85d3c3ec025d006dc5ed88de6e9bf4727b16b37
Aurora Ziobrowski <aziobrow@gmail.com>
2017-09-04 17:40:26 -0600

    finally got setup working

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -10,32 +10,15 @@ class GameBoard
   end
 
   def setup_board
-    board = Array.new(4).map {|row| Array.new(4)}
-    board = board.map.with_index do |row, index|
-      row = row.map.with_index do |game_square, index|
-        keys = create_keys(@rows[index])
-        game_square_key = [keys[index]]
-        game_square = Hash[game_square_key.map {|key| [key,nil]}]
-        row[index] = game_square
+    @rows.map do |letter|
+      @columns.map do |number|
+        key = letter + number
+        {key => nil}
       end
     end
-    board
   end
 
-  def setup_row(index)
-    keys = create_keys(@rows[index])
-    game_square = Hash.new
-    game_square[keys[index]] = nil
-  end
-
-  def create_keys(letter)
-    keys = []
-    @columns.each do |number|
-      keys << letter + number
-    end
-    keys
-  end
-
+  
   def win_condition
     @board.each do |row|
       row.each_key do |game_square|
--- test/game_board_test.rb
+++ test/game_board_test.rb
@@ -9,10 +9,9 @@ class GameBoardTest < Minitest::Test
     assert_instance_of Array, game_board.board
     assert_equal 4, game_board.board.count
 
-  #   assert_equal [{"A1"=>nil}, {"A2"=>nil}, {"A3"=>nil}, {"A4"=>nil}], game_board.board[0]
-  #   assert_equal [{"B1"=>nil}, {"B2"=>nil}, {"B3"=>nil}, {"B4"=>nil}], game_board.board[1]
-  #   assert_equal [{"C1"=>nil}, {"C2"=>nil}, {"C3"=>nil}, {"C4"=>nil}], game_board.board[2]
-  #   assert_equal [{"D1"=>nil}, {"D2"=>nil}, {"D3"=>nil}, {"D4"=>nil}], game_board.board[3]
-  p game_board.board
+    assert_equal [{"A1"=>nil}, {"A2"=>nil}, {"A3"=>nil}, {"A4"=>nil}], game_board.board[0]
+    assert_equal [{"B1"=>nil}, {"B2"=>nil}, {"B3"=>nil}, {"B4"=>nil}], game_board.board[1]
+    assert_equal [{"C1"=>nil}, {"C2"=>nil}, {"C3"=>nil}, {"C4"=>nil}], game_board.board[2]
+    assert_equal [{"D1"=>nil}, {"D2"=>nil}, {"D3"=>nil}, {"D4"=>nil}], game_board.board[3]
   end
 end
